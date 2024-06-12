c11180c11b43c91e025f0c8f37fa855129c008b2
shawshank-dulal <sashank.dulal@gmail.com>
2024-06-12 11:31:07 +0545

    initial upload all working

---
+++ default_script0.TOR5YD7V.mjs
@@ -0,0 +1,292 @@
+import {
+  B as I,
+  F as E,
+  R as z,
+  ba as b,
+  m as R,
+  o as u,
+  q as e,
+  r as _,
+  u as v,
+  z as k,
+} from "./chunk-YVMF765H.mjs";
+import { h as n, l as F } from "./chunk-6OCJ5Y37.mjs";
+import { c as a } from "./chunk-ELYU6EKT.mjs";
+var X = "default" in u ? R : u,
+  i = {},
+  w = X;
+i.createRoot = w.createRoot;
+i.hydrateRoot = w.hydrateRoot;
+var P = i.createRoot,
+  x = i.hydrateRoot;
+a.__framer_importFromPackage = (t, m) => () =>
+  n(k, {
+    error: 'Package component not supported: "' + m + '" in "' + t + '"',
+  });
+a.process = {
+  ...a.process,
+  env: { ...(a.process ? a.process.env : void 0), NODE_ENV: "production" },
+};
+I();
+a.__framer_events = a.__framer_events || [];
+function D() {
+  a.__framer_events.push(arguments);
+}
+function U(t) {
+  document.readyState === "interactive" || document.readyState === "complete"
+    ? t()
+    : document.addEventListener("readystatechange", () => {
+        document.readyState === "interactive" && t();
+      });
+}
+U(async function () {
+  try {
+    let t = {
+        augiA20Il: {
+          elements: {
+            AWIV0WPEg: "faq",
+            PkwIuVnJc: "insights",
+            sqeV2ApFX: "features",
+            zUh7qCsgk: "whygavie",
+          },
+          page: e(() =>
+            import("./375CUBMxZhqDiO8YpzOwSIGbUiK1-40YaZcy-f6-FxE.VZIQHFJP.mjs")
+          ),
+          path: "/",
+        },
+        qoRdNMoy4: {
+          elements: {},
+          page: e(() =>
+            import("./EYqRP0Gw0qbI5DvVwDWPEpWZtNFbadjdy9oKYVr_Ni4.7RZ4DWJU.mjs")
+          ),
+          path: "/pricing",
+        },
+        XL9MN6srX: {
+          elements: {},
+          page: e(() =>
+            import("./yhs0m0astHG1dvPfKFMQ-eZmhhajWMf5Aygr-6iuxLI.N34PTY3Z.mjs")
+          ),
+          path: "/company",
+        },
+        x9qAGHKO0: {
+          elements: {},
+          page: e(() =>
+            import("./4M0Vj-bh-V2Umzdy-2zoIKVtgSQhjz_AMXpKd9VPMBQ.V3M4MRGB.mjs")
+          ),
+          path: "/changelog",
+        },
+        YB72UBTJ0: {
+          elements: {},
+          page: e(() =>
+            import("./uKcswi9ZjlA7osB0Ib6X0kELJjEADThFZRf-1M4I5gc.GNDWKPAF.mjs")
+          ),
+          path: "/terms",
+        },
+        H4Mr_MPjv: {
+          elements: {},
+          page: e(() =>
+            import("./MF5lEkkUV5nkQkUOfxupUbOSmpBNnVbwR8LAKrnvZs8.NAC7BIOV.mjs")
+          ),
+          path: "/privacy",
+        },
+        jcAXvPy2y: {
+          elements: {},
+          page: e(() =>
+            import("./qIS-0V_A92RyFLlQDLyregLbVUzspZh2uF0jSWziCAc.JZ6SRKBU.mjs")
+          ),
+          path: "/careers",
+        },
+        TaRJMB6VK: {
+          elements: {},
+          page: e(() =>
+            import("./8vhfb4t8bJG9iMAyO3dIPQYJLv6IWMGW_7Y8R2iuiMc.6MVWOO4L.mjs")
+          ),
+          path: "/integrations",
+        },
+        MEiG5qyX4: {
+          elements: {},
+          page: e(() =>
+            import("./eAYkhzWJR2lhSvJ_Gt3f0w7TCW7idbaXLqR2pgvpV2Y.KESYLPPF.mjs")
+          ),
+          path: "/404",
+        },
+        aMIUCVUkf: {
+          elements: {},
+          page: e(() =>
+            import("./T9YmNXqRpRfBPj6n5Mc80t7s0z_YnfMS_YBn_WNUHMw.TNYNYNE2.mjs")
+          ),
+          path: "/contact",
+        },
+        iAQPfHZwd: {
+          elements: {},
+          page: e(() =>
+            import("./gaaz60W7q66OEwekH9rcaMhJfT9Ra1BQrOA1EzZuMFA.52WTBEGO.mjs")
+          ),
+          path: "/feature",
+        },
+        u4LUq2CwH: {
+          elements: {},
+          page: e(() =>
+            import("./CG_agzw-Yi5v-YPeOUSiTJynFKXJefKfeXmHk2_T7gw.RASQL5LD.mjs")
+          ),
+          path: "/feature-details",
+        },
+        kYxBde7R8: {
+          elements: {},
+          page: e(() =>
+            import("./Kmrh0WYQJE47FgnpUsVgnksjfaV6Esj2tZR-3vBqAAo.5HBT2WK2.mjs")
+          ),
+          path: "/case-study",
+        },
+        gC2FNmZhB: {
+          elements: {},
+          page: e(() =>
+            import("./9AKUURGS2f3ko2kfQYpxwqrI6znuBzlSOsTTfpM6FdY.QFSNA2TB.mjs")
+          ),
+          path: "/blog",
+        },
+        iEJeemgpP: {
+          collectionId: "uxvdtPxvo",
+          elements: {},
+          page: e(() =>
+            import("./uNGPjlgCm-x3v0P1B9TXH98qOJ3rbQgmaio4OYuPcpw.BTG3HBZZ.mjs")
+          ),
+          path: "/case-study/:uJ2rNjOWC",
+        },
+        EBmckUGfj: {
+          collectionId: "VXBMIwmlX",
+          elements: {},
+          page: e(() =>
+            import("./WJ1zxrjxxuTzP5fI0Wt5nREzupJlgLSUrZkyjHjkcvk.YIRH2CEF.mjs")
+          ),
+          path: "/blog/:aR6SdGqc2",
+        },
+      },
+      m = {
+        uxvdtPxvo: async () =>
+          (
+            await import(
+              "./data-module.Uj-krW8wodOfzRe7eJQDmH_-qOqYUsj9vMZ1pUEcWxs.F2RTSI2Y.mjs"
+            )
+          )?.utils,
+        VXBMIwmlX: async () =>
+          (
+            await import(
+              "./data-module.ZxmH9l_5dYIBqsyRI9J-ST9XgoKbIaZhDymbzEHoV1U.VY5HAGG2.mjs"
+            )
+          )?.utils,
+      },
+      y = [{ code: "en-US", id: "default", name: "English", slug: "" }],
+      M = e(() =>
+        import("./eAYkhzWJR2lhSvJ_Gt3f0w7TCW7idbaXLqR2pgvpV2Y.KESYLPPF.mjs")
+      ),
+      s = document.getElementById("main"),
+      o,
+      p,
+      l,
+      c,
+      d = !1;
+    if ("framerHydrateV2" in s.dataset) {
+      let r = JSON.parse(s.dataset.framerHydrateV2);
+      (o = r.routeId),
+        (p = r.localeId),
+        (l = r.pathVariables),
+        (c = r.breakpoints),
+        (d = !0);
+    } else {
+      let r = v(t, decodeURIComponent(location.pathname), !0, y);
+      (o = r.routeId), (p = r.localeId), (l = r.pathVariables);
+    }
+    let f = await t[o].page.preload();
+    t[o].page = f;
+    let g = !0,
+      V = n(z, {
+        RootComponent: f,
+        isWebsite: !0,
+        routeId: o,
+        pathVariables: l,
+        routes: t,
+        collectionUtils: m,
+        notFoundPage: M,
+        isReducedMotion: void 0,
+        localeId: p,
+        locales: y,
+        preserveQueryParams: !1,
+        enableImproveInpDuringHydration: g,
+        addHydrationMarkers: d,
+      }),
+      H = n(E, { children: V, value: { imgSizesWorkaroundEnabled: !1 } }),
+      h = n(_, {
+        children: H,
+        value: {
+          global: {
+            enter: {
+              opacity: 0,
+              rotate: 0,
+              rotate3d: !1,
+              rotateX: 0,
+              rotateY: 0,
+              scale: 1,
+              transition: {
+                damping: 30,
+                delay: 0,
+                duration: 0.2,
+                ease: [0.27, 0, 0.51, 1],
+                mass: 1,
+                stiffness: 400,
+                type: "tween",
+              },
+              x: "0px",
+              y: "0px",
+            },
+            exit: {
+              opacity: 0,
+              rotate: 0,
+              rotate3d: !1,
+              rotateX: 0,
+              rotateY: 0,
+              scale: 1,
+              transition: {
+                damping: 30,
+                delay: 0,
+                duration: 0.2,
+                ease: [0.27, 0, 0.51, 1],
+                mass: 1,
+                stiffness: 400,
+                type: "tween",
+              },
+              x: "0px",
+              y: "0px",
+            },
+          },
+          routes: { augiA20Il: {} },
+        },
+      });
+    d
+      ? (performance.mark("framer-rewrite-breakpoints-start"),
+        b(c),
+        a.__framer_onRewriteBreakpoints?.(c),
+        performance.mark("framer-rewrite-breakpoints-end"),
+        performance.measure(
+          "framer-rewrite-breakpoints",
+          "framer-rewrite-breakpoints-start",
+          "framer-rewrite-breakpoints-end"
+        ),
+        F(() => {
+          performance.mark("framer-hydration-start"),
+            g && a.__FRAMER_TURN_OFF_REACT_EVENT_HANDLING__?.(),
+            x(s, h);
+        }))
+      : P(s).render(h);
+  } catch (t) {
+    throw (
+      (D("published_site_load_error", {
+        message: String(t),
+        stack:
+          t instanceof Error && typeof t.stack == "string" ? t.stack : null,
+      }),
+      t)
+    );
+  }
+});
+//# sourceMappingURL=default_script0.TOR5YD7V.mjs.map
